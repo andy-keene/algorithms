18a3d74a0fa0a4a757761a2b44e30e8c84a710c3
andy-keene <keene.andy.t@gmail.com>
2017-01-29 20:38:44 -0800

    Structure algorithm run for data capture - 1st data set

--- main.cpp
+++ main.cpp
@@ -1,55 +1,125 @@
 #include <iostream>
 #include <random>
+#include <fstream>
 
+using namespace std;
 
 void mergeSort(int *, int, int);
 void merge(int  *, int, int, int);
 bool isSorted(int *, int);
-using namespace std;
+float ticksToSec(clock_t);
+int * initRandArr(int, uniform_int_distribution<int>&, mt19937&);             //must be passed by refference 
+void printArr(int *, int);
+
+
 
 int main(){
 
     //Non deterministic random number generator & instantiation
     random_device device;
     mt19937 generator(device());
-    uniform_int_distribution<int> size_dist(0,10000);
+    uniform_int_distribution<int> size_dist(0,1000);
     clock_t t;
-    
-    int length = 10000;
-    int * arr = new (nothrow) int[length];    
 
-    int maxLength = 0;                          //Data collection variables
-    int startLength = 0;
-    int averageSize = 0;
-    int stepSize = 0;
 
-    if(arr == NULL){                            //in case memory can't be allocated
-        return -1;
+    ofstream msFile;
+    msFile.open("/Users/andykeene/Downloads/mergesortData.csv");                              //msFile = mergesort File
+    if (!msFile.is_open()){
+        cout << "Failed to open file";
+        exit(-1);
+    } else {
+        msFile << "\"Length\"," << "\"Ticks\"," << "\"Seconds\"\n"; 
     }
+
+    
+    int length = 20;
+    //int * arr = initRandArr(length, size_dist, generator);   //new (nothrow) int[length];    
+    int * arr = NULL;
+    int * barr = initRandArr(length, size_dist, generator);
+    
+
+
+    int maxLength = 1000;                          //Data collection variables
+    int startLength = 5;
+    int averageSize = 7;
+    int stepSize = 5;
+
+
+    
+    //Data collection
+    for (int i = startLength; i < maxLength; i += stepSize){
+
+        arr = initRandArr(i, size_dist, generator);     //init array of length i
+
+        if(arr == NULL){                                //in case memory can't be allocated
+            return -1;
+        }
+
+        //printArr(arr, i);                               //print for testing
+        
+        t = clock();                                    //time algorithm Merge Sort
+        mergeSort(arr, 0, i-1);
+        t = clock() - t;
+
+        if (!isSorted(arr, i)){
+            cout << "error: sort failed\n";
+            exit(-1);
+        }
+
+        cout << "Mergesort for length " << i << " took " << t << " clicks, "
+             << ticksToSec(t) << " seconds\n";
+
+        msFile << i << "," << t << "," << ticksToSec(t) << endl;         //Print length, ticks, seconds to file
+
+        delete[] arr;                                   //free mem
+    }
+    
+
+    msFile.close();
+    //delete[] arr;
+    //printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+    return 0;
+}
+
+
+// prints array as "Length[n]: 1, 2, 3, ... n"
+void printArr(int * arr, int length)
+{
+    cout << "Length [" << length << "]: ";
 
     for (int i = 0; i < length; i++){
-    	arr[i] = size_dist(generator);
+        cout << arr[i] << ", ";
     }
 
-    //Data collection
+    cout << endl;
+}
 
-    cout << "--- Before sort --- \n";
 
-    t = clock();
 
-    mergeSort(arr, 0, length-1);
+//Precondition: Given length of array to create, a device and mt19937 psuedo-random number generator 
+//Post-condition: Retuns pointer to an array initialized with random integers for the given length 
+//                (size_dist and generator MUST be passed by refference to avoid psuedo-random path repetition)
+int * initRandArr(int length, uniform_int_distribution<int> &size_dist, mt19937 &generator)
+{
+    int * arr = new (nothrow) int[length];
 
-    t = clock() - t;
+    if (!arr){                       //failed to generate list of length n
+        return NULL;
+    } else {
+        
+        for(int i = 0; i < length; i++){
+            arr[i] = size_dist(generator);
+        }
 
-    if (!isSorted(arr, length)){
-        cout << "Failed to sort";
+        return arr;
     }
-    cout << "--- After sort --- \n";
+}
 
-   
-    delete[] arr;
-    printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
-    return 0;
+
+// Converts clock object's tick-time to seconds
+float ticksToSec(clock_t t)
+{
+    return ((float)t)/CLOCKS_PER_SEC;
 }
 
 /* --- Holding Tank ----
@@ -59,12 +129,29 @@ int main(){
         cout << arr[i] << endl;
     }
 
+    -- test different array allocations
+    if( arr ){
+
+        cout << "Array A\n";
+
+        for (int i = 0; i < length; i++){
+            cout << arr[i] << endl;
+        }
+
+        cout << "Array B\n";
+        for (int i = 0; i < length; i++){
+            cout << barr[i] << endl;
+        }
+    } else {
+        cout << "init array FAILED";
+    }
+
 
 */
 
-//Precondition:
-//Postcondition: 
-//Note:
+//Precondition: Arrary arr[b...t]
+//Postcondition: Array arr is sorted in ascending order
+//Note: Ave of O( n*log(n) )
 void mergeSort(int * arr, int b, int t)
 {
     if (b < t){
@@ -73,11 +160,11 @@ void mergeSort(int * arr, int b, int t)
         mergeSort(arr, m+1, t);
         merge(arr, b, m, t);
 
-    } else {
-        //cout << "Base case: arr[" << b << "] = " << arr[b] << "\n";
-    }
+    } //else { array of one is already sorted }
 }
 
+//Precondition: Given sorted sub-arrays [b..m] and [m+1..t] of Array arr
+//Postcondition: Sorted sub-array [b...t]
 void merge(int * arr, int b, int m, int t)
 {
     int * barr = new (nothrow) int[t-b+1];          //allocate mem for array b s.t it can hold elements b through t
@@ -111,6 +198,8 @@ void merge(int * arr, int b, int m, int t)
     delete[] barr;                             //free array b mem
 }
 
+//Precondition: Initialized array, arr[0...length-1]
+//Postcondition: True if arr is sorted, flase if else
 bool isSorted(int * arr, int length)
 {
 
